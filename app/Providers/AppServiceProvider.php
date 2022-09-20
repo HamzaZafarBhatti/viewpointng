318fc7f591c7184475cc8b829e5edac28fe2624c
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-09-21 01:36:40 +0500

    mining done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\AffliateProfit;
 use App\Models\Logo;
 use App\Models\Setting;
 use App\Models\Ui;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -32,27 +36,28 @@ class AppServiceProvider extends ServiceProvider
         //
         View::composer('*', function ($view) {
             if (Auth::check()) {
-                // $user = User::find(Auth::user()->id);
-                // $extraction = Extraction::whereUserId(auth()->user()->id)->where('status', 0)->latest('id')->first();
-                // if ($extraction) {
-                //     $end_date = Carbon::parse($extraction->end_datetime)->format("Y-m-d H:i:s");
-                //     Log::info($end_date);
-                //     if (Carbon::now() > $end_date) {
-                //         Log::info('end');
-                //         $extraction->update(['status' => 1]);
-                //         if ($extraction->status == 1) {
-                //             // Log::info('if2');
-                //             $user->update(['extraction_balance' => $user->extraction_balance + $extraction->profit]);
-                //             $extraction->update(['status' => 2]);
-                //         }
-                //     }
-                // }
+                $user = User::find(Auth::user()->id);
+                $extraction = AffliateProfit::whereUserId(auth()->user()->id)->where('status', 0)->latest('id')->first();
+                if ($extraction) {
+                    $end_date = Carbon::parse($extraction->end_datetime)->format("Y-m-d H:i:s");
+                    Log::info($end_date);
+                    if (Carbon::now() > $end_date) {
+                        Log::info('end');
+                        $extraction->update(['status' => 1]);
+                        if ($extraction->status == 1) {
+                            // Log::info('if2');
+                            $user->update(['balance' => $user->balance + $extraction->profit]);
+                            $extraction->update(['status' => 2]);
+                        }
+                    }
+                }
 
                 // $notifications = Notification::whereIsRead(0)->where('user_id', auth()->user()->id)->latest()->limit(7)->get();
                 // $all_notifications = Notification::whereIsRead(0)->where('user_id', auth()->user()->id)->latest()->get();
                 // $view->with('notifications', $notifications);
                 // $view->with('all_notifications', $all_notifications);
                 $view->with('user_proof', Auth::user()->show_popup);
+                $view->with('user', Auth::user());
             }
         });
         $data['set'] = Setting::first();
