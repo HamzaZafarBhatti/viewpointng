9e623c92b7ce41956e96d1a0f4be10043bfb446f
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-11-12 11:30:19 +0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -84,12 +84,14 @@ class UserController extends Controller
     {
         // return $request;
         // return date('Y-m-d');
-        $today = Carbon::now();
-        $transaction_date = Carbon::create($today->year, $today->month, 28);
-        // return json_encode($today < $transaction_date);
-        if($today < $transaction_date && ($today->hour >= 7 && $today->hour <= 9)) {
-            return back()->with('alert', 'You can cashout your Video Earning Points every 28th of the Month.');
-        }
+
+        // $today = Carbon::now();
+        // $transaction_date = Carbon::create($today->year, $today->month, 28);
+        // // return json_encode($today < $transaction_date);
+        // if($today < $transaction_date && ($today->hour >= 7 && $today->hour <= 9)) {
+        //     return back()->with('alert', 'You can cashout your Video Earning Points every 28th of the Month.');
+        // }
+
         $validator = Validator::make($request->all(), [
             'amount' => 'required',
             'details' => 'required',
@@ -167,11 +169,12 @@ class UserController extends Controller
     {
         // return $request;
         // return date('Y-m-d')
-        $today = Carbon::now();
-        $day_of_week = $today->format('l');
-        if($day_of_week != 'Sunday' && $day_of_week != 'Wednesday') {
-            return back()->with('alert', 'You can request for your Ref Earning Balance Payout every Sundays and Wednesdays to your BANK Account only.');
-        }
+
+        // $today = Carbon::now();
+        // $day_of_week = $today->format('l');
+        // if($day_of_week != 'Sunday' && $day_of_week != 'Wednesday') {
+        //     return back()->with('alert', 'You can request for your Ref Earning Balance Payout every Sundays and Wednesdays to your BANK Account only.');
+        // }
 
         $validator = Validator::make($request->all(), [
             'amount' => 'required',
@@ -248,17 +251,18 @@ class UserController extends Controller
         // if($today < $transaction_date) {
         //     return back()->with('alert', 'You can cashout your Video Earning Points every 28th of the Month.');
         // }
-        $last_wd = Withdraw::whereUser_id(auth()->user()->id)->whereType(2)->latest()->first();
-        // return $last_wd;
-        if ($last_wd) {
-            $end = Carbon::parse($last_wd->created_at);
-            $now = Carbon::now();
-            $length = $end->diffInDays($now);
-            // return $length;
-            if ($length < 1) {
-                return back()->with('alert', 'You have already requested this payment.');
-            }
-        }
+
+        // $last_wd = Withdraw::whereUser_id(auth()->user()->id)->whereType(2)->latest()->first();
+        // // return $last_wd;
+        // if ($last_wd) {
+        //     $end = Carbon::parse($last_wd->created_at);
+        //     $now = Carbon::now();
+        //     $length = $end->diffInDays($now);
+        //     // return $length;
+        //     if ($length < 1) {
+        //         return back()->with('alert', 'You have already requested this payment.');
+        //     }
+        // }
 
         $validator = Validator::make($request->all(), [
             'amount' => 'required',
